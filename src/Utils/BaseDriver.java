ac87c81d344eef5693bc5e36e45ce4637e95ae44
hmk10 <halitmk@gmail.com>
2021-08-12 22:00:49 -0400

    Base Driver modification for cross-browser testing

--- src/Utils/BaseDriver.java
+++ src/Utils/BaseDriver.java
@@ -1,11 +1,14 @@
 package Utils;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.NoSuchElementException;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Parameters;
 
 public class BaseDriver {
 
@@ -14,11 +17,18 @@ public class BaseDriver {
     protected WebDriver driver;
 
     @BeforeMethod(alwaysRun = true)
-    public void setup(){
-        System.setProperty("webdriver.chrome.driver", "/Library/Selenium/chromedriver/chromedriver");
-        driver = new ChromeDriver();
-        driver.manage().window().maximize();
+    @Parameters({"browser"})
+    public void setup(String browser){
 
+        if (browser.equalsIgnoreCase("chrome")){
+            System.setProperty("webdriver.chrome.driver", "/Library/Selenium/chromedriver/chromedriver");
+            driver = new ChromeDriver();
+        } else if (browser.equalsIgnoreCase("firefox")){
+            System.setProperty("webdriver.gecko.driver", "/Library/Selenium/geckodriver/geckodriver");
+            driver = new FirefoxDriver();
+        }
+
+        driver.manage().window().maximize();
         driver.get("http://opencart.abstracta.us/index.php?route=common/home");
 
         WebElement myAccountButton = driver.findElement(By.xpath("//a[@title='My Account']"));
@@ -27,11 +37,16 @@ public class BaseDriver {
         WebElement loginButton = driver.findElement(By.linkText("Login"));
         loginButton.click();
 
-        WebElement advancedButton = driver.findElement(By.xpath("//button[@id='details-button']"));
-        advancedButton.click();
+        try {
+            // this part is only for chrome browser - not firefox
+            WebElement advancedButton = driver.findElement(By.xpath("//button[@id='details-button']"));
+            advancedButton.click();
 
-        WebElement proceedLink = driver.findElement(By.xpath("//a[@id='proceed-link']"));
-        proceedLink.click();
+            WebElement proceedLink = driver.findElement(By.xpath("//a[@id='proceed-link']"));
+            proceedLink.click();
+        } catch (NoSuchElementException e){
+            System.out.println("This part is only for Chrome!");
+        }
 
         WebElement username = driver.findElement(By.id("input-email"));
         username.sendKeys("test@techno.com");
